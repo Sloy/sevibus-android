b215a234ba4f784ba50defd6315e3cdf76c19104
Rafa Vázquez <rafa.vazsan@gmail.com>
2024-04-17 20:20:24 +0200

    Base project creation

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    //id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.sloydev.sevibus"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sloydev.sevibus"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.navigation)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+
+    implementation(libs.coroutines.android)
+    implementation(libs.coroutines.playServices)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.play.services.auth)
+
+    //implementation(libs.room.runtime)
+    //implementation(libs.room.ktx)
+    //kapt(libs.room.compiler)
+
+
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.cokoin)
+    implementation(libs.koin.cokoinViewmodel)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.junit)
+}
---
+++ app/src/main/java/com/sloydev/sevibus/App.kt
@@ -0,0 +1,12 @@
+package com.sloydev.sevibus
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.sloydev.sevibus.ui.theme.SevibusTheme
+
+@Composable
+fun App() {
+    SevibusTheme {
+        Text("Hello world")
+    }
+}
