9e55b3cb9f1fdeaf2dc3c1390136f050915d5f35
Rafa Vázquez <rafa.vazsan@gmail.com>
2024-04-19 15:43:29 +0200

    Improved design of Lines screen

--- app/src/main/java/com/sloydev/sevibus/feature/lines/LinesScreen.kt
+++ app/src/main/java/com/sloydev/sevibus/feature/lines/LinesScreen.kt
@@ -1,20 +1,23 @@
 package com.sloydev.sevibus.feature.lines
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,15 +45,13 @@ private fun LinesScreen(state: LinesState, onLineClick: (Line) -> Unit = {}) {
                 CenterAlignedTopAppBar(
                     title = { Text(text = stringResource(id = R.string.navigation_lines)) },
                 )
-                LazyColumn {
+                LazyColumn(Modifier.padding(horizontal = 16.dp)) {
                     Stubs.lineTypes.forEach { lineType ->
                         val linesOfType = state.lines.filter { it.type == lineType }
                         if (linesOfType.isNotEmpty()) {
                             item { LineTypeTitle(lineType) }
-                            items(linesOfType) { line ->
-                                LineItem(line, onLineClick)
-                            }
-                            item { Spacer(Modifier.size(16.dp)) }
+                            item { LinesCard(linesOfType, onLineClick) }
+                            item { Spacer(Modifier.size(32.dp)) }
                         }
                     }
                 }
@@ -60,28 +61,38 @@ private fun LinesScreen(state: LinesState, onLineClick: (Line) -> Unit = {}) {
 }
 
 @Composable
-private fun LineTypeTitle(lineType: String) {
-    Text(
-        lineType,
-        style = MaterialTheme.typography.titleMedium,
-        modifier = Modifier.padding(start = (16 + 8).dp)
+fun LinesCard(lines: List<Line>, onLineClick: (Line) -> Unit) {
+    Card {
+        Column {
+            lines.forEachIndexed { index, line ->
+                LineItem(line, onLineClick)
+                if(index < lines.lastIndex){
+                    HorizontalDivider(Modifier.padding(horizontal = 16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun LineItem(line: Line, onLineClick: (Line) -> Unit) {
+    ListItem(
+        modifier = Modifier.clickable { onLineClick(line) },
+        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+        headlineContent = { Text(line.description) },
+        leadingContent = {
+            LineIndicatorMedium(line)
+        },
     )
 }
 
 @Composable
-private fun LineItem(it: Line, onLineClick: (Line) -> Unit) {
-    Card(
-        onClick = { onLineClick(it) },
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
-    ) {
-        ListItem(
-            tonalElevation = 8.dp,
-            headlineContent = { Text(it.description) },
-            leadingContent = {
-                LineIndicatorMedium(it)
-            },
-        )
-    }
+private fun LineTypeTitle(lineType: String) {
+    Text(
+        lineType,
+        style = MaterialTheme.typography.titleMedium,
+        modifier = Modifier.padding(start = (16 + 8).dp, bottom = 8.dp)
+    )
 }
 
 
