a559c42b8b0fe52ecec510d9e2046d12e8152111
Rafa Vázquez <rafa.vazsan@gmail.com>
2024-04-26 17:22:14 +0200

    Improve design for route timeline items

--- app/src/main/java/com/sloydev/sevibus/feature/linestops/component/StopTimelineElement.kt
+++ app/src/main/java/com/sloydev/sevibus/feature/linestops/component/StopTimelineElement.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,17 +20,20 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sloydev.sevibus.Stubs
 import com.sloydev.sevibus.domain.model.LineSummary
 import com.sloydev.sevibus.domain.model.Stop
+import com.sloydev.sevibus.domain.model.description1
+import com.sloydev.sevibus.domain.model.description2
 import com.sloydev.sevibus.feature.linestops.component.ListPosition.End
 import com.sloydev.sevibus.feature.linestops.component.ListPosition.Middle
 import com.sloydev.sevibus.feature.linestops.component.ListPosition.Start
 import com.sloydev.sevibus.ui.components.LineIndicatorSmall
 import com.sloydev.sevibus.ui.theme.AlexGreen
-import com.sloydev.sevibus.ui.theme.AlexPurple
+import com.sloydev.sevibus.ui.theme.AlexGreyIcons
 import com.sloydev.sevibus.ui.theme.SevTheme
 
 
@@ -48,10 +52,20 @@ fun StopTimelineElement(
                 .padding(end = 8.dp)
         ) {
             ListItem(
+                modifier = Modifier.padding(vertical = 8.dp),
                 colors = ListItemDefaults.colors(containerColor = Color.Transparent),
-                headlineContent = { Text(stop.description) },
-                overlineContent = { Text(text = stop.code.toString()) },
-                supportingContent = { SupportingLines(stop.lines) },
+                headlineContent = {
+                    Column {
+                        Text(stop.description1, style = MaterialTheme.typography.bodyLarge, fontWeight = FontWeight.Bold)
+                        stop.description2?.let {
+                            Text(it, style = MaterialTheme.typography.bodyMedium, color = AlexGreyIcons)
+                        }
+                    }
+                },
+                supportingContent = {
+                    Text("Parada " + stop.code, style = MaterialTheme.typography.labelSmall, fontWeight = FontWeight.Light)
+                },
+                trailingContent = { SupportingLines(stop.lines) },
             )
         }
     }
