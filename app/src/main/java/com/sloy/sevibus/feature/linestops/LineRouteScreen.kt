dd655b7282ebfbfd5261f35cf4f565e1f76874ff
Rafa Vázquez <rafa.vazsan@gmail.com>
2024-09-22 18:24:35 +0200

    Header for Line Route screen

--- app/src/main/java/com/sloy/sevibus/feature/linestops/LineRouteScreen.kt
+++ app/src/main/java/com/sloy/sevibus/feature/linestops/LineRouteScreen.kt
@@ -1,22 +1,18 @@
 package com.sloy.sevibus.feature.linestops
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -32,9 +28,8 @@ import com.sloy.sevibus.domain.model.primary
 import com.sloy.sevibus.feature.linestops.component.ListPosition
 import com.sloy.sevibus.feature.linestops.component.StopTimelineElement
 import com.sloy.sevibus.navigation.NavigationDestination
-import com.sloy.sevibus.ui.components.LineIndicatorSmall
+import com.sloy.sevibus.ui.components.LineIndicatorMedium
 import com.sloy.sevibus.ui.components.RouteTabsSelector
-import com.sloy.sevibus.ui.components.SevTopAppBar
 import com.sloy.sevibus.ui.preview.ScreenPreview
 import com.sloy.sevibus.ui.theme.SevTheme
 import org.koin.androidx.compose.koinViewModel
@@ -52,7 +47,11 @@ fun NavGraphBuilder.lineStopsRoute(navController: NavController, onRouteSelected
 }
 
 @Composable
-private fun LineRouteScreen(lineId: LineId, onStopClick: (Stop) -> Unit, onRouteSelected: (route: Route) -> Unit) {
+private fun LineRouteScreen(
+    lineId: LineId,
+    onStopClick: (Stop) -> Unit,
+    onRouteSelected: (route: Route) -> Unit
+) {
     val viewModel: LineRouteViewModel = koinViewModel { parametersOf(lineId) }
     val state by viewModel.state.collectAsState()
     LineRouteScreen(
@@ -62,46 +61,30 @@ private fun LineRouteScreen(lineId: LineId, onStopClick: (Stop) -> Unit, onRoute
             onRouteSelected(it)
         },
         onStopClick = onStopClick,
-        embedded = true,
     )
 }
 
 @Composable
-fun LineRouteScreen(
+private fun LineRouteScreen(
     state: LineRouteScreenState,
     onRouteSelected: (route: Route) -> Unit,
     onStopClick: (Stop) -> Unit,
-    embedded: Boolean = true,
 ) {
     Column {
-        if (!embedded) {
-            SevTopAppBar(title = {
-                if (state is LineRouteScreenState.Content) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        LineIndicatorSmall(state.line, Modifier.padding(end = 8.dp))
-                        Text(state.line.description, maxLines = 1, overflow = TextOverflow.Ellipsis)
-                    }
-                }
-            },
-                navigationIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null,
-                            tint = SevTheme.colorScheme.onSurface,
-                        )
-                    }
-                }
-            )
-        }
         if (state is LineRouteScreenState.Content) {
+            Row(Modifier.padding(horizontal = 16.dp)) {
+                LineIndicatorMedium(state.line, Modifier.padding(end = 8.dp))
+                Text(state.line.description, style = SevTheme.typography.headingSmall, maxLines = 1)
+            }
             if (state.line.routes.size > 1) {
                 RouteTabsSelector(
                     route1 = state.line.routes[0], route2 = state.line.routes[1], selected = state.selectedRoute.id, onRouteClicked = {
                         onRouteSelected(it)
                     },
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier.padding(horizontal = 16.dp).padding(top = 16.dp)
                 )
             }
+            HorizontalDivider(Modifier.padding(vertical = 16.dp))
         }
 
         when (state) {
@@ -122,7 +105,7 @@ private fun RouteContent(
     line: Line,
     onStopClick: (Stop) -> Unit
 ) {
-    LazyColumn {
+    LazyColumn(contentPadding = PaddingValues(bottom = 16.dp)) {
         itemsIndexed(stops) { index, stop ->
             StopTimelineElement(
                 stop,
@@ -140,11 +123,27 @@ private fun RouteContent(
 
 @Preview
 @Composable
-private fun Preview() {
+private fun PreviewWithRoutes() {
     ScreenPreview {
         LineRouteScreen(
             state = LineRouteScreenState.Content.Full(
                 line = Stubs.lines[2],
+                stops = Stubs.stops.shuffled(),
+                selectedRoute = Stubs.lines[2].routes.first(),
+            ),
+            onRouteSelected = {},
+            onStopClick = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewWithoutRoutes() {
+    ScreenPreview {
+        LineRouteScreen(
+            state = LineRouteScreenState.Content.Full(
+                line = Stubs.lines.first { it.routes.size == 1 },
                 stops = Stubs.stops.shuffled(),
                 selectedRoute = Stubs.lines[2].routes.first(),
             ),
