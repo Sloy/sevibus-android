ad0e07a3e0abc1baba9957313436a1fd6a40bbe4
Rafa Vázquez <rafa.vazsan@gmail.com>
2025-06-11 19:29:54 +0200

    chore: update major versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.icons)
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.morfly.bottomsheet)
 
     implementation(libs.accompanist.permissions)
--- app/src/main/java/com/sloy/sevibus/infrastructure/extensions/KoinExtensions.kt
+++ app/src/main/java/com/sloy/sevibus/infrastructure/extensions/KoinExtensions.kt
@@ -2,7 +2,6 @@ package com.sloy.sevibus.infrastructure.extensions
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.platform.LocalView
 import org.koin.compose.currentKoinScope
 import org.koin.compose.koinInject
 import org.koin.core.parameter.ParametersDefinition
@@ -16,12 +15,17 @@ import org.koin.core.scope.Scope
  * Prevents errors due to Koin not being initialized in Preview mode.
  */
 @Composable
+@Suppress("UndeclaredKoinUsage")
 inline fun <reified T> koinInjectOnUI(
     qualifier: Qualifier? = null,
     scope: Scope? = null,
     noinline parameters: ParametersDefinition? = null,
 ): T? = if (!LocalInspectionMode.current) {
-    koinInject<T>(qualifier, scope ?: currentKoinScope(), parameters)
+    if (parameters != null) {
+        koinInject<T>(qualifier, scope ?: currentKoinScope(), parameters)
+    } else {
+        koinInject<T>(qualifier, scope ?: currentKoinScope())
+    }
 } else {
     null
 }
